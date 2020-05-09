3af9885fe0ea62de5919ad0212da99edfa683e5a
dimmoon69 <pldimitry@yandex.ru>
2020-05-09 12:56:37 +0300

    Обновления после мержа

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from backend.news import views
 
 urlpatterns = [
-    path("add/", views.CreatePostView.as_view(), name="add-post"),
+    path("add/", views.CreatePostView.as_view(), name="add_post"),
     path("<slug:slug>/", views.SinglePost.as_view(), name="single_post"),
     path("", views.PostList.as_view(), name="news"),
 ]
